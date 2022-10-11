c7ab99b2f45f8ecd4c0e234dc083ae8718d9f54d
Pedro Silva <pedro.silva@cbyk.com.br>
2022-10-11 18:18:32 -0300

    feat: email service

---
+++ src/pages/api/email/send.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { TRANSPORT_CONFIG } from '@/config/email.config'
+import nodemailer from 'nodemailer'
+import NextCors from 'nextjs-cors'
+
+type ResponseData = { message: string }
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    await NextCors(req, res, {
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        optionsSuccessStatus: 200,
+        origin: '*'
+    })
+
+    try {
+        const transporter = nodemailer.createTransport(TRANSPORT_CONFIG)
+        await transporter.sendMail(req.body)
+
+        res.status(200).json({ message: 'Email enviado com sucesso' })
+    } catch (error) {
+        res.status(400).json({ message: 'Ocorreu um erro ao enviar email' })
+    }
+}

2f615d5cbe5ef88abc3c94dd082c29bc7f9e2445
Pedro Silva <pedro.silva@cbyk.com.br>
2022-10-18 08:38:58 -0300

    feat: next configs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
-const withImages = require('next-images')
+/** @type {import('next').NextConfig} */
 
-module.exports = withImages({
-    images: { disableStaticImages: true }
-})
+const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins')
+
+console.log(`[NEXT_CONFIG]: ${process.env.NODE_ENV}`)
+const nextConfig = {
+    reactStrictMode: true,
+    trailingSlash: process.env.NODE_ENV.trim() == 'development'
+}
+
+module.exports = withPlugins([
+    [nextConfig],
+    [
+        withImages,
+        {
+            esModule: true,
+            images: { disableStaticImages: true }
+        }
+    ]
+])

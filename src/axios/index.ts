4b40cdb9a1efa98ed6b0ebcc5bc968deb1309f00
Pedro Silva <pedro.silva@cbyk.com.br>
2022-10-25 23:43:06 -0300

    fix: update api url

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: 'https://papamike.vercel.app/api',
+    baseURL: 'https://papamike-ui.vercel.app/api',
     headers: {
         Accept: 'application/json',
         'content-type': 'application/json'
